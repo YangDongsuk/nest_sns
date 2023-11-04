9f6a3e24cd17a4305dd3b8f990e229f1bb3c8192
dongsuk <scienceyds@naver.com>
2023-11-04 21:49:16 +0900

    [feature] basemodel 적용

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,17 +1,17 @@
+import { BaseModel } from 'src/common/entity/base.entity';
 import { UsersModel } from 'src/users/entities/users.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
-export class PostsModel {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class PostsModel extends BaseModel {
   // 1) UserModel과 연동한다. ForeignKey를 이용해서
   // 2) null이 될 수 없다.
   @ManyToOne(() => UsersModel, (user) => user.posts, { nullable: false })
