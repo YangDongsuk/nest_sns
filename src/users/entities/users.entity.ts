7ba9830f918fda83ee9839cd4ab819d5e55da06a
dongsuk <scienceyds@naver.com>
2023-11-05 15:25:19 +0900

    [feature] 앱모듈에 ClassSerializerInterceptor 적용

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -18,8 +18,13 @@ import {
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity()
+// 만약에 이게 보안상의 이유로 노출되면 안되는 정보라면
+// @Exclude()를 붙여주면 된다.
+// 그리고 보여주고 싶은 정보만 @Expose()를 붙여주면 된다.
+// 기존의 반대로 하면 된다.
 export class UsersModel extends BaseModel {
   // 1) 길이가 20을 넘지 않는다.
   // 2) 유일무이한 값이다.
@@ -32,6 +37,13 @@ export class UsersModel extends BaseModel {
     message: lengthValidationMessage,
   })
   nickname: string;
+
+  // 원래 겟터는 클라이언트에게 전달되지 않는다.
+  // 하지만 @Expose()를 붙여주면 클라이언트에게 전달된다.
+  // @Expose()
+  // get nicknameAndEmail(): string {
+  //   return `${this.nickname}/${this.email}`;
+  // }
 
   @Column({
     unique: true,
@@ -52,6 +64,21 @@ export class UsersModel extends BaseModel {
   @Length(3, 8, {
     message: lengthValidationMessage,
   })
+  /**
+   * Exclude
+   *
+   * Request
+   * frontend -> backend
+   * plain object (JSON) -> class instance (dto)
+   *
+   * Response
+   * backend -> frontend
+   * class instance (dto) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance로 변환할 때만 적용 -> Request일 때만
+   * toPlainOnly -> plain object로 변환할 때만 적용 -> Response일 때만
+   */
+  @Exclude({ toPlainOnly: true })
   password: string;
 
   @Column({
